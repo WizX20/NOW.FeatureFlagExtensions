ffce9761c08e49f96f12006bf2f7f3e04325906f
Wouter Paap <nerdsonwaves@outlook.com>
2022-05-23 13:26:15 +0200

    Adds optional argument for the `apiVersioningOptions`

--- src/ApiVersioning.Swagger/Extensions/IServiceCollectionExtensions.cs
+++ src/ApiVersioning.Swagger/Extensions/IServiceCollectionExtensions.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
+using NOW.FeatureFlagExtensions.ApiVersioning.Configuration;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace NOW.FeatureFlagExtensions.ApiVersioning.Swagger.Extensions
 {
     public static class IServiceCollectionExtensions
     {
-        public static void ConfigureApiVersioningSwaggerGenOptions(this IServiceCollection services)
+        public static void ConfigureApiVersioningSwaggerGenOptions(this IServiceCollection services, ApiVersioningOptions? apiVersioningOptions = null)
         {
+            if (apiVersioningOptions != null)
+            {
+                services.TryAddSingleton(apiVersioningOptions);
+            }
+
             services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
         }
     }
